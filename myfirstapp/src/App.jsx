506f0130046afeac2aee0fa0331e6d3f7add21f6
srihariraj <srihariatom@gmail.com>
2025-11-02 17:27:38 +0530

    hello world program

--- myfirstapp/src/App.jsx
+++ myfirstapp/src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Helloworld from './components/Helloworld.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>
-        EMPTY PAGE
-      </h1>
+      <Helloworld />
     </>
   )
 }
